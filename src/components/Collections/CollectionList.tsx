e8ff386b31f75cec84aa1efda61e5a9ce7986dfa
Adonis0923 <91833579+Adonis0923@users.noreply.github.com>
2022-03-24 00:39:20 +0900

    top collection list

--- src/components/Collections/CollectionList.tsx
+++ src/components/Collections/CollectionList.tsx
@@ -1,8 +1,33 @@
+import { Container, Row, Col, Stack, Image } from 'react-bootstrap';
+import { CollectionListItem } from './CollectionListItem'
+import './collections.style.scss';
 const CollectionList = () => {
   return (
-    <>
-      CollectionList
-    </>
+    <Container>
+      <h1 className='title'>Top collections this week</h1>
+      <Container className="p-4">
+        <Row>
+          <Col md={4} sm={12} xs={12}>
+            <CollectionListItem />
+            <CollectionListItem />
+            <CollectionListItem />
+            <CollectionListItem />
+          </Col>
+          <Col md={4} sm={12} xs={12}>
+            <CollectionListItem />
+            <CollectionListItem />
+            <CollectionListItem />
+            <CollectionListItem />
+          </Col>
+          <Col md={4} sm={12} xs={12}>
+            <CollectionListItem />
+            <CollectionListItem />
+            <CollectionListItem />
+            <CollectionListItem />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
   )
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,31 @@
-import { Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Landing, CollectionList, TopShelfDrops, Card } from '../components';
 
 const Home = () => {
   return (
-    <Row>
+    <Row className='home'>
       <Landing />
       <TopShelfDrops />
       <CollectionList />
       <Row className='nft-carousel'>
-
+        <Container className="p-5">
+          <h1 className='title'>Checkout what's trending</h1>
+          <Row className="p-3">
+            <Col md={8} className='m-auto'>
+              <Row>
+                <Col md={4}>
+                  <Card />
+                </Col>
+                <Col md={4}>
+                  <Card />
+                </Col>
+                <Col md={4}>
+                  <Card />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
       </Row>
       <Row className='categories' >
 
