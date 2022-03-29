c0354f9aba7c8173ff971a40538bbc7aabed5de4
Adonis0923 <91833579+Adonis0923@users.noreply.github.com>
2022-03-29 19:08:02 +0900

    align items

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,18 +1,90 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button, Stack, Image, ListGroup } from 'react-bootstrap';
 import '../styles/collection.scss';
+import { CollectionItem } from '../components';
 
 function Collection() {
   return (
-    <Container className="collections p-0" fluid>
-      <Row className="collections-landing">
-        <div className="collections-landing-bg" />
-        <Col md={12} className="collections-landing-title">
-          <h1>Explore collections</h1>
-        </Col>
+    <Container className="collection p-0" fluid>
+      <Row className="collection-landing">
+        <div className="collection-landing-bg" />
+        <div className="collection-badge" />
       </Row>
-      <Row>
+      <Row className="collection-description">
         <h1>LoopingRings</h1>
-        <p></p>
+        <p>
+          Created By: <span className="text-info mx-2">Monster Magnet</span>
+          <Button className="mx-2">
+            <Image src="/icons/twitter.svg" width={15} height={15} />
+          </Button>
+          <Button>
+            <Image src="/icons/discord.svg" width={15} height={15} />
+          </Button>
+        </p>
+        <Stack direction="horizontal" className="collection-params">
+          <Stack direction="horizontal">
+            <div>
+              <h1>333</h1>
+              <p>items</p>
+            </div>
+            <div>
+              <h1>285</h1>
+              <p>owners</p>
+            </div>
+          </Stack>
+          <Stack direction="horizontal">
+            <div>
+              <h1>
+                <Image src={`/icons/eth.png`} className="mb-1" />
+                <span>0.32</span>
+              </h1>
+              <p>floor price</p>
+            </div>
+            <div>
+              <h1>
+                <Image src={`/icons/eth.png`} className="mb-1" />
+                <span>30.6</span>
+              </h1>
+              <p>volume trated</p>
+            </div>
+          </Stack>
+        </Stack>
+      </Row>
+      <Row className="collection-tab">
+        <Stack direction="horizontal">
+          <div>
+            <Image src="/icons/item.png" />
+            <p>Items</p>
+          </div>
+          <div>
+            <Image src="/icons/Graph Poly.png" />
+            <p>Activity</p>
+          </div>
+        </Stack>
+      </Row>
+      <Row className="mb-3 collection-body">
+        <Col lg={3}>
+          <ListGroup as="ol">
+            <ListGroup.Item as="li">
+              <span>Trait</span>
+            </ListGroup.Item>
+            <ListGroup.Item as="li">
+              <span>Trait</span>
+            </ListGroup.Item>
+            <ListGroup.Item as="li">
+              <span>Trait</span>
+            </ListGroup.Item>
+            <ListGroup.Item as="li">
+              <span>Trait</span>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col lg={9}>
+          <Row className="pt-4">
+            {new Array(8).fill(22).map((item: number, key: number) => {
+              return <CollectionItem key={key} id={key + 1} />;
+            })}
+          </Row>
+        </Col>
       </Row>
     </Container>
   );
