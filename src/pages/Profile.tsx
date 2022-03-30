d5ead8a204a50b83128acd188daaa26a372b76f8
Adonis0923 <91833579+Adonis0923@users.noreply.github.com>
2022-03-31 02:22:49 +0900

    remove dropdown list from user icon in navbar

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,16 @@
+import '../styles/profile.scss';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+
 const Profile = () => {
-  return <>Profile</>;
+  return (
+    <Container>
+      <Row>
+        <Col md={8}>
+          <Image src={`/images/profile.png`} />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export { Profile };
