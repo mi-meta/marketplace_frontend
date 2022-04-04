af5c3df99d32c81e0481be556dd7061b23ade593
Adonis0923 <91833579+Adonis0923@users.noreply.github.com>
2022-04-04 20:19:27 +0900

    fix h1 font

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -17,71 +17,7 @@ import { DropdownComp } from '../components';
 function Create() {
   return (
     <Container className="create">
-      <Row className="justify-content-center mt-5">
-        <Col lg={9}>
-          <h1>Choose Blockchain</h1>
-          <p>
-            Select the blockchain that suits your needs best. You will be required to sign in to
-            create your NFT.
-          </p>
-          <Row className="create-network">
-            <Col lg={4} md={6} sm={12}>
-              <Card>
-                <Card.Body className="m-auto text-center mb-5">
-                  <h1>Ethereum</h1>
-                  <Image src={`/icons/ethereum-67-107.png`} className="mt-4" />
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col lg={4} md={6} sm={12}>
-              <Card>
-                <Card.Body className="m-auto text-center mb-5">
-                  <h1>Polygon</h1>
-                  <Image src={`/icons/polygon-88-88.png`} className="mt-4" />
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col lg={4} md={6} sm={12}>
-              <Card>
-                <Card.Body className="m-auto text-center mb-5">
-                  <h1>Solana</h1>
-                  <Image src={`/icons/solana-79-79.png`} className="mt-4" />
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="justify-content-center mt-5">
-        <Col lg={9}>
-          <h1>Choose Type</h1>
-          <p>
-            Choose “Single” for one of a kind or “Multiple” if you want to sell one collectible
-            multiple times
-          </p>
-          <Row className="create-type">
-            <Col lg={4} md={6} sm={12}>
-              <Card>
-                <Card.Body className="m-auto text-center mb-5">
-                  <h1>Single</h1>
-                  <Image src={`/icons/erc721-logo.png`} className="mt-4" />
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col lg={4} md={6} sm={12}>
-              <Card>
-                <Card.Body className="m-auto text-center mb-5">
-                  <h1>Editions</h1>
-                  <Image src={`/icons/erc1155-logo.png`} className="mt-4" />
-                  {/* <Image src={`/icons/erc1155-logo.png`} className="mt-4" />
-                  <Image src={`/icons/erc1155-logo.png`} className="mt-4" /> */}
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="create-nft justify-content-center">
+      <Row className="create-nft justify-content-center mt-5">
         <Col lg={9}>
           <h1>Create NFT on Ethereum</h1>
           <Row>
@@ -143,7 +79,6 @@ function Create() {
                 <BootstrapSwitchButton checked={true} />
               </Stack>
               <div className="create-nft-trait">
-                <p>List your new NFT on the marketplace</p>
                 <Stack direction="horizontal" className="create-nft-price-type" gap={3}>
                   <div className="text-center">
                     <p>Fixed price</p>
@@ -184,7 +119,7 @@ function Create() {
                   <FormControl />
                   <DropdownComp items={['a', 'b']} />
                 </Stack>
-                <p>
+                <p className="mt-3">
                   Service fee <span className="text-light">2.5%</span>
                 </p>
                 <p>
@@ -285,6 +220,70 @@ function Create() {
                   Cancel!
                 </Button>
               </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-5">
+        <Col lg={9}>
+          <h1>Choose Blockchain</h1>
+          <p>
+            Select the blockchain that suits your needs best. You will be required to sign in to
+            create your NFT.
+          </p>
+          <Row className="create-network">
+            <Col lg={4} md={6} sm={12}>
+              <Card>
+                <Card.Body className="m-auto text-center mb-5">
+                  <h1>Ethereum</h1>
+                  <Image src={`/icons/ethereum-67-107.png`} className="mt-4" />
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <Card>
+                <Card.Body className="m-auto text-center mb-5">
+                  <h1>Polygon</h1>
+                  <Image src={`/icons/polygon-88-88.png`} className="mt-4" />
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <Card>
+                <Card.Body className="m-auto text-center mb-5">
+                  <h1>Solana</h1>
+                  <Image src={`/icons/solana-79-79.png`} className="mt-4" />
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-5">
+        <Col lg={9}>
+          <h1>Choose Type</h1>
+          <p>
+            Choose “Single” for one of a kind or “Multiple” if you want to sell one collectible
+            multiple times
+          </p>
+          <Row className="create-type">
+            <Col lg={4} md={6} sm={12}>
+              <Card>
+                <Card.Body className="m-auto text-center mb-5">
+                  <h1>Single</h1>
+                  <Image src={`/icons/erc721-logo.png`} className="mt-4" />
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <Card>
+                <Card.Body className="m-auto text-center mb-5">
+                  <h1>Editions</h1>
+                  <Image src={`/icons/erc1155-logo.png`} className="mt-4" />
+                  {/* <Image src={`/icons/erc1155-logo.png`} className="mt-4" />
+                  <Image src={`/icons/erc1155-logo.png`} className="mt-4" /> */}
+                </Card.Body>
+              </Card>
             </Col>
           </Row>
         </Col>
